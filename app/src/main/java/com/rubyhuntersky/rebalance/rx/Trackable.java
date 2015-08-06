37ab2df853ffc5cb9e3597fbdc37772228ace37f
Jeffrey Yu <wehjin@gmail.com>
2015-08-05 18:52:16 -0700

    Add AllocationViewActivity.

---
+++ app/src/main/java/com/rubyhuntersky/rebalance/rx/Trackable.java
@@ -0,0 +1,9 @@
+package com.rubyhuntersky.rebalance.rx;
+
+/**
+ * @author wehjin
+ * @since 8/5/15.
+ */
+
+public interface Trackable {
+}
---
+++ app/src/main/java/com/rubyhuntersky/rebalance/rx/TrackableActivity.java
@@ -0,0 +1,11 @@
+package com.rubyhuntersky.rebalance.rx;
+
+import android.app.Activity;
+
+/**
+ * @author wehjin
+ * @since 8/5/15.
+ */
+
+public class TrackableActivity extends Activity {
+}
